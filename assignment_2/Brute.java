7e489b50d7b3997cc5147f5d0ee1ea9aa9e9a3fc
Satya Aditi <satyaaditi93@gmail.com>
2018-06-06 10:49:20 -0400

    commit final changes

--- assignment_2/Brute.java
+++ assignment_2/Brute.java
@@ -3,73 +3,115 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package daa.assignment2;
+package daa_assignment2;
 
 /**
  *
  * @author satya
  */
 import java.util.*;
-import java.io.*;
+import java.awt.*;
+import daa.assignment2.StdDraw;
 
 
 public class Brute {
-    public static void main(String args[]) throws FileNotFoundException
+   
+    public static void main(String args[]) 
     { 
+        try{
         int array_size=0;
         int points_x = 0;
         int points_y = 0;
+        int x=0,y=0;
+        int max_x=0, max_y=0;
+        int min_x=Integer.MAX_VALUE, min_y=Integer.MAX_VALUE;        
+        // Loop iterators
         int i = 0;
+        int l = 0;
+        int m = 0;
+        int j = 0;
+        int d = 0;
         Point p,q,r,s;
-        double slope_pq,slope_pr,slope_ps;
-        Point[] a;
-        
-        File file=new File("C:/Users/satya/Desktop/input.txt");
-        Scanner in = new Scanner(new FileInputStream(file));
+        ArrayList<Point> list = new ArrayList<Point>();
+        double slope_pq,slope_pr,slope_ps; //Declaring slopes for lines joining  p,q,r,s.
+        Point[] a; //Declaring array of points
+        java.io.File file=new java.io.File("input8.txt");
+        //You can also use "args[0]" in place of the input file name, to give command line arguments at runtime.
+
+        Scanner in = new Scanner(file);
      
-        //FileInputStream in = new FileInputStream(file);
         if (in.hasNext()) {
             array_size = in.nextInt();
-        }
+        }     //Checking the number of pairs of points in the given input file and creating an array of that size
         a = new Point[array_size];
         while(in.hasNext() && array_size>0)
         {
           points_x = in.nextInt();
           points_y = in.nextInt();
           a[i] = new Point(points_x,points_y);
+          max_x = Math.max(max_x, points_x);
+          min_x = Math.min(min_x, points_x);
+          max_y = Math.max(max_y, points_y);
+          min_y = Math.min(min_y, points_y);
+          //This is to draw the axis for the points to be displayed,the aix co-ordinates should in the range of the input values.
           i++;     
         }
-        
-        for
-                for
-                        if compareTo >= 0
-                                continue
-                        slope_pq = slopeTo                        
-                        for
-                                if compareTo >= 0
-                                   continue
-                                slope_pr = slopeTo
-                                if slope_pq != slope_pr
-                                    continue
-                                for
-                                        if conpareTo >= 0
-                                           continue
-                                        slope_ps = slopeTo
-                                        if slope_pq != slope_ps
-                                            continue
-                                        else
-                                            store this as p,q,r,s as solution
-                                end for
-                        end for
-                end for
-        end for
-        int j=0;
-        while(j<i)
-        {
-            System.out.println(a[j].toString());
-            j++;
-
+        StdDraw.setXscale(0, Math.max(max_x, max_y)+500);//Setting the scale for the X-axis
+        StdDraw.setYscale(0, Math.max(max_x, max_y)+500);
+        StdDraw.setPenColor(Color.BLACK);
+        StdDraw.line(0, 0, Math.max(max_x, max_y), 0);//Drawing the x-axis
+        StdDraw.line(0, 0, 0, Math.max(max_x, max_y));        
+        int radius = 200;
+        for (i=0; i<a.length;i++) {
+            a[i].draw(radius); //It will draw the points of radius 200.
+        }
+        //While input.txt has next Lines ,the first point on the left is taken as x and second point in right is taken as y and these points x and y are stored in the array
+        int length=a.length;
+      
+        for(l=0 ;l<length;l++) {
+            p=a[l];//p can be all the points in the array because of the for loop
+            for(m =0; m<a.length;m++) {
+                q=a[m]; //Second Iteration,q now contains all the points
+                if (p.compareTo(q) >= 0) {
+                    continue; /*Using compareTo function from Point.java ,comparing points p and q ,here it will check all the points from first iteration to all the points in second iteration
+                    , It will check whether the x co-ordinates and y-cordinates are equal or greater or less and accordingly return valuess,if its greater than 0 then it will terminate the for loop and go ahead.*/
+                    
+                }
+                slope_pq = p.slopeTo(q); //Calculating slope of the line joining p and q 
+                for(j=0;j<a.length;j++) {
+                    r=a[j];
+                    if (q.compareTo(r) >= 0) {
+                        continue;
+                    }
+                    slope_pr = p.slopeTo(r);
+                    if (slope_pq != slope_pr) {
+                        continue;
+                    }//If the slope of pq is less than slope pr then it will terminate the loop there and wont consider further points
+                    for(d=0;d<a.length;d++) {
+                        s=a[d];
+                        if (r.compareTo(s) >= 0) {
+                            continue;
+                        }
+                        slope_ps = p.slopeTo(s);
+                        if (slope_pq != slope_ps) {
+                            continue;
+                        }//Now all 3 slopes are considered we know ,thus we got the 4 points we know.
+                       
+                        System.out.println(p+"->"+q+"->"+r+"->"+s);
+                        p.drawTo(s);
+                        /*store this  p,q,r,s as solution;*/
+                        }
+                        }
+                }
         }
         
+      
+        
     }
+    
+    catch(Exception e)
+    {
+             e.printStackTrace();
+    }
+}
 }
--- assignment_2/Point.java
+++ assignment_2/Point.java
@@ -1,9 +1,11 @@
+package daa_assignment2;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package daa.assignment2;
+
 
 /**
  *
@@ -19,24 +21,42 @@ package daa.assignment2;
  *
  *************************************************************************/
 
+import daa.assignment2.StdDraw;
 import java.util.Comparator;
+import java.awt.*;
+import javax.swing.*;
+
+class DemoPanel extends JPanel
+{
+  public DemoPanel()
+  {
+    setPreferredSize( new Dimension(20000, 20000) );
+    setBackground( Color.WHITE );    
+  }  
+}
+//another class for declaring JPanel
 
 public class Point implements Comparable<Point> {
 
-    // compare points by slope
-    public final Comparator<Point> SLOPE_ORDER = (Point o1, Point o2) -> {
-        double slope1 = o1.slopeTo(o2); //calculates slope of point o1 and o2
-        if (slope1 > 0) {
-            return 1;
-        } else {
-            return 0;
-        }
-    } //Defining comparator slope_order
-    ;       // YOUR DEFINITION HERE
-    Point x0,y0,x1,y1,x2,y2;
-    double slope;
-    private final int x;                              // x coordinate
-    private final int y;                              // y coordinate
+    // compare points by slope,defining a comparator SLOPE_ORDER which compares slopes between two points and returns 1 if equal.
+
+   public final Comparator<Point> SLOPE_ORDER = (Point o1, Point o2) -> {
+       double slope1=o1.slopeTo(o2);
+       if (slopeTo(o1) > slopeTo(o2)) {
+           return 1;
+       } else {
+           return -1;
+       }
+  
+   };
+   //This comparator is to compare points using compareTo function and return the compareTo return value
+   public final Comparator<Point> POINT_ORDER = (Point o1, Point o2) -> {
+       return o1.compareTo(o2);
+   } ;      
+   
+    double slope = 0;
+    public final int x;                              // x coordinate
+    public final int y;                              // y coordinate
 
     // create the point (x, y)
     public Point(int x, int y) {
@@ -44,22 +64,31 @@ public class Point implements Comparable<Point> {
         this.y = y;
     }
 
+   
     // plot this point to standard drawing
-    public void draw() {
-        /* YOUR CODE HERE */
+    public void draw(int r) {
+        StdDraw.setPenColor(Color.RED);
+        StdDraw.filledCircle(this.x,this.y, r);
+        
     }
 
     // draw line between this point and that point to standard drawing
     public void drawTo(Point that) {
-         /* YOUR CODE HERE */
+        StdDraw.setPenColor(Color.BLUE);
+        StdDraw.line(this.x, this.y, that.x, that.y);
+         
     }
 
     // slope between this point and that point
     public double slopeTo(Point that) {
-    slope=(that.y-this.y)/(that.x-this.x); //calculating slope between this point and that point
-    
-     return slope;
-       
+       if(that.x!=this.x) {
+           slope = (((double)(that.y-this.y))/((double)(that.x-this.x))); //calculating slope between this point and that point and castng it as double.
+       } else if(that.x==this.x && that.y!=this.y) {
+           slope = Double.POSITIVE_INFINITY; //If it is a vertical line
+       } else if(that.x==this.x && that.y==this.y) {
+           slope=Double.NEGATIVE_INFINITY; //If it is a degenerate line
+       }
+        return slope;
     }
 
     /* is this point lexicographically smaller than that one?
@@ -72,18 +101,25 @@ public class Point implements Comparable<Point> {
     public int compareTo(Point that) {
        if(this.y<that.y || (this.y==that.y && this.x<that.x))
        {
-           return -1;
+           return -1; 
        }
-       else if (this.y>that.y)
+       else if (this.y==that.y && this.x==that.x)
        {
-           return 1;
+           return 0;
        }
-       return 0;//If doesnt any any of the above conditions it will return 0 showing that this.
-        }
+       return 1;//If doesnt satisfy any of the above conditions it will return 0 showing that this.y ,that.y and this.x and that.x are all equal.
+    }
 
     // return string representation of this point (x, y)
     @Override
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
-}
+
+ 
+        
+        
+    }
+    
+    
+
